18bfe9c0b51b0f09e8ea0142e02df4aeead80995
Taejeong Bae <bintriz@gmail.com>
2019-01-03 11:38:27 -0600

    Add timezone option

--- post_install.sh
+++ post_install.sh
@@ -1,16 +1,41 @@
 #!/bin/bash
 
-yum -y update
+ARGUMENT_LIST=(
+    "timezone"
+)
+
+# read arguments
+opts=$(getopt \
+    --longoptions "$(printf "%s:," "${ARGUMENT_LIST[@]}")" \
+    --name "$(basename "$0")" \
+    --options "" \
+    -- "$@"
+)
+
+eval set --$opts
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --timezone)
+            TZ=$2
+            shift 2
+            ;;
+
+        *)
+            break
+            ;;
+    esac
+done
 
 # Installing packages
+yum -y update
 yum -y install libcurl-devel gsl-devel # for bcftools
 yum -y install readline-devel sqlite-devel # for python
 yum -y install gcc72-c++ libXpm-devel xauth # for root
 yum -y install tmux parallel emacs # etc 
 
 # Timezone
-if [[ -e /shared/config/timezone ]]; then
-    TZ=$(</shared/config/timezone)
+if [[ ! -z "$TZ" ]]; then
     sed -i '/ZONE/s|UTC|'$TZ'|' /etc/sysconfig/clock
     ln -sf /usr/share/zoneinfo/$TZ /etc/localtime
     reboot
