ec611aac868498d668a9d45a601d098c11ae9fb6
Taejeong Bae <bintriz@gmail.com>
2019-08-12 16:50:02 +0900

    Create configure_sge.sh

---
+++ configure_sge.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+source /etc/*cluster/cfnconfig
+
+if [[ $cfn_node_type = "MasterServer" ]] && qconf -sp threaded >/dev/null 2>&1; then
+    qconf -Ap <<END
+pe_name            threaded
+slots              99999
+user_lists         NONE
+xuser_lists        NONE
+start_proc_args    NONE
+stop_proc_args     NONE
+allocation_rule    $pe_slots
+control_slaves     FALSE
+job_is_first_task  TRUE
+urgency_slots      min
+accounting_summary TRUE
+qsort_args         NONE
+END
+    qconf -mattr queue pe_list threaded all.q
+fi
