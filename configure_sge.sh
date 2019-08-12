9b9596f1db9e4c61ac584fc524e6afe5f039a61a
Taejeong Bae <bintriz@gmail.com>
2019-08-12 17:04:22 +0900

    Update configure_sge.sh

--- configure_sge.sh
+++ configure_sge.sh
@@ -3,19 +3,20 @@
 source /etc/*cluster/cfnconfig
 
 if [[ $cfn_node_type = "MasterServer" ]] && ! qconf -sp threaded >/dev/null 2>&1; then
-    qconf -Ap <<END
+    cat >/shared/config/threaded.conf <<END
 pe_name            threaded
 slots              99999
 user_lists         NONE
 xuser_lists        NONE
 start_proc_args    NONE
 stop_proc_args     NONE
-allocation_rule    $pe_slots
+allocation_rule    \$pe_slots
 control_slaves     FALSE
 job_is_first_task  TRUE
 urgency_slots      min
 accounting_summary TRUE
 qsort_args         NONE
 END
+    qconf -Ap /shared/config/threaded.conf
     qconf -mattr queue pe_list threaded all.q
 fi
